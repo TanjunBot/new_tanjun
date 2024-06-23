e68fc50662031b98e2819ab6354b2a4f8da5a2a6
EntchenEric <eseidel2004@gmail.com>
2024-06-23 12:27:03 +0200

    Updated numericStringParser to support more calculations

--- utility.py
+++ utility.py
@@ -805,95 +805,91 @@ class NumericStringParser(object):
         self.exprStack.append(toks[0])
 
     def pushUMinus(self, strg, loc, toks):
-        if toks and toks[0] == "-":
-            self.exprStack.append("unary -")
+        if toks and toks[0] == '-':
+            self.exprStack.append('unary -')
 
     def __init__(self):
-        """
-        expop   :: '^'
-        multop  :: '*' | '/'
-        addop   :: '+' | '-'
-        integer :: ['+' | '-'] '0'..'9'+
-        atom    :: PI | E | real | fn '(' expr ')' | '(' expr ')'
-        factor  :: atom [ expop factor ]*
-        term    :: factor [ multop factor ]*
-        expr    :: term [ addop term ]*
-        """
         point = Literal(".")
         e = CaselessLiteral("E")
-        fnumber = Combine(
-            Word("+-" + nums, nums)
-            + Opt(point + Opt(Word(nums)))
-            + Opt(e + Word("+-" + nums, nums))
-        )
+        fnumber = Combine(Word("+-" + nums, nums) +
+                          Optional(point + Optional(Word(nums))) +
+                          Optional(e + Word("+-" + nums, nums)))
         ident = Word(alphas, alphas + nums + "_$")
-        plus = Literal("+")
-        minus = Literal("-")
-        mult = Literal("*")
-        div = Literal("/")
-        lpar = Literal("(").suppress()
-        rpar = Literal(")").suppress()
+
+        plus, minus, mult, div = map(Literal, "+-*/")
+        lpar, rpar = map(Literal, "()")
         addop = plus | minus
         multop = mult | div
         expop = Literal("^")
-        pi = CaselessLiteral("PI")
+
         expr = Forward()
         atom = (
-            (
-                Opt(oneOf("- +"))
-                + (ident + lpar + expr + rpar | pi | e | fnumber).setParseAction(
-                    self.pushFirst
-                )
-            )
-            | Opt(oneOf("- +")) + Group(lpar + expr + rpar)
-        ).setParseAction(self.pushUMinus)
-        # by defining exponentiation as "atom [ ^ factor ]..." instead of
-        # "atom [ ^ atom ]...", we get right-to-left exponents, instead of left-to-right
-        # that is, 2^3^2 = 2^(3^2), not (2^3)^2.
+            (Optional("-") + (ident + lpar + expr + rpar | fnumber)).setParseAction(self.pushFirst) |
+            (lpar + expr.suppress() + rpar).setParseAction(self.pushUMinus)
+        )
+
         factor = Forward()
         factor << atom + ZeroOrMore((expop + factor).setParseAction(self.pushFirst))
+
         term = factor + ZeroOrMore((multop + factor).setParseAction(self.pushFirst))
         expr << term + ZeroOrMore((addop + term).setParseAction(self.pushFirst))
-        # addop_term = ( addop + term ).setParseAction( self.pushFirst )
-        # general_term = term + ZeroOrMore( addop_term ) | OneOrMore( addop_term)
-        # expr <<  general_term
+
         self.bnf = expr
-        # map operator symbols to corresponding arithmetic operations
-        epsilon = 1e-12
+        self.exprStack = []
+
+        # Function map
+        self.fn = {
+            "sin": math.sin,
+            "cos": math.cos,
+            "tan": math.tan,
+            "asin": math.asin,
+            "acos": math.acos,
+            "atan": math.atan,
+            "sinh": math.sinh,
+            "cosh": math.cosh,
+            "tanh": math.tanh,
+            "asinh": math.asinh,
+            "acosh": math.acosh,
+            "atanh": math.atanh,
+            "log": math.log,
+            "log10": math.log10,
+            "log2": math.log2,
+            "exp": math.exp,
+            "abs": abs,
+            "trunc": math.trunc,
+            "round": round,
+            "sgn": lambda a: abs(a) > 1e-12 and cmp(a, 0) or 0,
+            "sqrt": math.sqrt,
+            "factorial": math.factorial,
+            "degrees": math.degrees,
+            "radians": math.radians,
+        }
+
+        # Operator map
         self.opn = {
             "+": operator.add,
             "-": operator.sub,
             "*": operator.mul,
             "/": operator.truediv,
-            "^": operator.pow,
-        }
-        self.fn = {
-            "sin": math.sin,
-            "cos": math.cos,
-            "tan": math.tan,
-            "exp": math.exp,
-            "abs": abs,
-            "trunc": lambda a: int(a),
-            "round": round,
-            "sgn": lambda a: abs(a) > epsilon and cmp(a, 0) or 0,
+            "^": operator.pow
         }
 
     def evaluateStack(self, s):
         op = s.pop()
-        if op == "unary -":
+        if op == 'unary -':
             return -self.evaluateStack(s)
         if op in "+-*/^":
             op2 = self.evaluateStack(s)
             op1 = self.evaluateStack(s)
             return self.opn[op](op1, op2)
         elif op == "PI":
-            return math.pi  # 3.1415926535
+            return math.pi
         elif op == "E":
-            return math.e  # 2.718281828
+            return math.e
         elif op in self.fn:
             return self.fn[op](self.evaluateStack(s))
         elif op[0].isalpha():
-            return 0
+            raise Exception(f"Invalid identifier: {op}")
         else:
             return float(op)
 
