1d0492ea9d4ff2cc315bf2090af4bdf6256d7f30
EntchenEric <eseidel2004@gmail.com>
2024-07-26 16:22:35 +0200

    added command to get avatar decoration of user

---
+++ commands/utility/avatarDecoration.py
@@ -0,0 +1,30 @@
+from utility import commandInfo, tanjunEmbed
+from localizer import tanjunLocalizer
+import discord
+
+async def avatarDecoration(commandInfo: commandInfo, user: discord.Member):
+    if not user.avatar_decoration:
+        embed = tanjunEmbed(
+            title=tanjunLocalizer.localize(
+                commandInfo.locale, "commands.utility.avatarDecoration.no_decoration.title"
+            ),
+            description=tanjunLocalizer.localize(
+                commandInfo.locale,
+                "commands.utility.avatarDecoration.no_decoration.description",
+            ),
+        )
+        await commandInfo.reply(embed=embed)
+        return
+    
+    embed = tanjunEmbed(
+        title=tanjunLocalizer.localize(
+            commandInfo.locale, "commands.utility.avatarDecoration.title",
+            user=user.display_name,
+        ),
+        description=tanjunLocalizer.localize(
+            commandInfo.locale,
+            "commands.utility.avatarDecoration.description",
+        ),
+    )
+    embed.set_image(url=user.avatar_decoration.url)
+    await commandInfo.reply(embed=embed)
--- extensions/utility.py
+++ extensions/utility.py
@@ -12,6 +12,7 @@ from commands.utility.autopublish import autopublish_remove as autopublishRemove
 from commands.utility.avatar import avatar as avatarCommand
 from commands.utility.banner import banner as bannerCommand
 from commands.utility.feedback import feedback as feedbackCommand
+from commands.utility.avatarDecoration import avatarDecoration as avatarDecorationCommand
 
 class MessageTrackingCommands(discord.app_commands.Group):
     @app_commands.command(
@@ -159,6 +160,32 @@ class utilityCommands(discord.app_commands.Group):
             user = ctx.user
 
         await bannerCommand(commandInfo=commandInfo, user=user)
+
+    @app_commands.command(
+        name=app_commands.locale_str("utility_avatardecoration_name"),
+        description=app_commands.locale_str("utility_avatardecoration_description"),
+    )
+    @app_commands.describe(
+        user="The user to get the avatar decoration of.",
+    )
+    async def avatardecoration(self, ctx, user: discord.Member = None):
+        await ctx.response.defer()
+        commandInfo = utility.commandInfo(
+            user=ctx.user,
+            channel=ctx.channel,
+            guild=ctx.guild,
+            command=ctx.command,
+            locale=ctx.locale,
+            message=ctx.message,
+            permissions=ctx.permissions,
+            reply=ctx.followup.send,
+            client=ctx.client,
+        )
+
+        if not user:
+            user = ctx.user
+
+        await avatarDecorationCommand(commandInfo=commandInfo, user=user)
 
     @app_commands.command(
         name=app_commands.locale_str("utility_feedback_name"),
