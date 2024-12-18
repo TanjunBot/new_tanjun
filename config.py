15f161229fa981c47665d2cc9e44804234566b57
EntchenEric <eseidel2004@gmail.com>
2024-12-18 16:32:53 +0100

    Updated gitignore and removed ignored files

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import dotenv_values
 
 config = dotenv_values(".env")
 
-version = "0.7.11"
+version = "0.7.12"
 token = config["token"]
 applicationId = config["applicationId"]
 adminIds = list(config["adminIds"])
