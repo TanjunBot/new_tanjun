078bd09d6a338808815d1bafeb07092abfa09b48
Eric Seidel <eseidel2004@gmail.com>
2026-04-14 07:31:00 +0200

    Update config.py to support coolify

--- config.py
+++ config.py
@@ -1,29 +1,29 @@
-# Unused imports:
-# import os
+import os
+from dotenv import load_dotenv
 
-from dotenv import dotenv_values
-
-config: dict[str, str | None] = dotenv_values(".env")
+load_dotenv()
 
 version = "1.0.5"
-token = config["token"]
-applicationId = config["applicationId"]
-admin_ids_str: str | None = config.get("adminIds")
+token = os.environ.get("token")
+applicationId = os.environ.get("applicationId")
+
+admin_ids_str: str | None = os.environ.get("adminIds")
 adminIds: list[int] = [int(id) for id in admin_ids_str.split(",")] if admin_ids_str is not None else []
+
 activity = "Tanjun {version}"
-database_ip = config["database_ip"]
-database_password = config["database_password"]
-database_user = config["database_user"]
-database_schema = config["database_schema"]
-tenorAPIKey = config["tenorAPIKey"]
-tenorCKey = config["tenorCKey"]
-GithubAuthToken = config["GithubAuthToken"]
-ImgBBApiKey = config["ImgBBApiKey"]
-openAiKey = config["openAIKey"]
-bytebin_url = config["bytebin_url"]
-bytebin_password = config["bytebin_password"]
-bytebin_username = config["bytebin_username"]
-brawlstarsToken = config["brawlstarsToken"]
-twitchSecret = config["twitchSecret"]
-twitchId = config["twitchId"]
-prefix = config["prefix"]
+database_ip = os.environ.get("database_ip")
+database_password = os.environ.get("database_password")
+database_user = os.environ.get("database_user")
+database_schema = os.environ.get("database_schema")
+tenorAPIKey = os.environ.get("tenorAPIKey")
+tenorCKey = os.environ.get("tenorCKey")
+GithubAuthToken = os.environ.get("GithubAuthToken")
+ImgBBApiKey = os.environ.get("ImgBBApiKey")
+openAiKey = os.environ.get("openAIKey")
+bytebin_url = os.environ.get("bytebin_url")
+bytebin_password = os.environ.get("bytebin_password")
+bytebin_username = os.environ.get("bytebin_username")
+brawlstarsToken = os.environ.get("brawlstarsToken")
+twitchSecret = os.environ.get("twitchSecret")
+twitchId = os.environ.get("twitchId")
+prefix = os.environ.get("prefix")
