4b1ebc1bc688ef079c196ff141a42acc4482f07e
Eric Seidel <eseidel2004@gmail.com>
2026-04-17 22:37:59 +0200

    temporarely disable rembg command

--- commands/image/background.py
+++ commands/image/background.py
@@ -3,7 +3,7 @@ from io import BytesIO
 
 import discord
 from PIL import Image
-from rembg import remove as removeBackground  # type: ignore[import-not-found]
+# from rembg import remove as removeBackground  # type: ignore[import-not-found]
 
 import utility
 from localizer import tanjunLocalizer
@@ -29,14 +29,19 @@ async def background(commandInfo: utility.CommandInfo, image: discord.Attachment
 
     image = await image.read()  # type: ignore[assignment]
     image = Image.open(io.BytesIO(image))  # type: ignore[assignment, arg-type]
-    image = removeBackground(image)
+#    image = removeBackground(image)
+    # Background removal functionality is temporarily disabled
+    # You can re-enable this when rembg is properly installed
+    # image = removeBackground(image)
 
+    # Background removal is temporarily disabled
     buffer = BytesIO()
     image.save(buffer, format="png")  # type: ignore[call-arg, unused-coroutine]
     buffer.seek(0)
+
     embed = utility.tanjunEmbed(
-        title=tanjunLocalizer.localize(str(commandInfo.locale), "commands.image.background.success.title"),
-        description=tanjunLocalizer.localize(str(commandInfo.locale), "commands.image.background.success.description"),
+        title=tanjunLocalizer.localize(str(commandInfo.locale), "commands.image.background.disabled.title"),
+        description=tanjunLocalizer.localize(str(commandInfo.locale), "commands.image.background.disabled.description"),
     )
     embed.set_image(url="attachment://image.png")
     await commandInfo.reply(embed=embed, file=discord.File(fp=buffer, filename="image.png"))
