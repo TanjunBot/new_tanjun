85da2a3f229bf962f85768fd2c4cf1391e6fabe3
EntchenEric <78521072+EntchenEric@users.noreply.github.com>
2026-04-17 20:38:23 +0000

    style: autoformat and lint Python code with Ruff

--- commands/image/background.py
+++ commands/image/background.py
@@ -3,8 +3,8 @@ from io import BytesIO
 
 import discord
 from PIL import Image
-# from rembg import remove as removeBackground  # type: ignore[import-not-found]
 
+# from rembg import remove as removeBackground  # type: ignore[import-not-found]
 import utility
 from localizer import tanjunLocalizer
 
@@ -29,7 +29,7 @@ async def background(commandInfo: utility.CommandInfo, image: discord.Attachment
 
     image = await image.read()  # type: ignore[assignment]
     image = Image.open(io.BytesIO(image))  # type: ignore[assignment, arg-type]
-#    image = removeBackground(image)
+    #    image = removeBackground(image)
     # Background removal functionality is temporarily disabled
     # You can re-enable this when rembg is properly installed
     # image = removeBackground(image)
