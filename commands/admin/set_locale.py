e1b08ab4d0903aa8cc477b75b2b9630abf8d7d7a
EntchenEric <eric@tanjun.gg>
2026-06-01 19:11:38 +0200

    fix: rename function param locale to locale_str/lang to prevent variable shadowing in set_locale and num2word

--- commands/admin/set_locale.py
+++ commands/admin/set_locale.py
@@ -2,14 +2,14 @@ from locale_keys import locale
 import discord
 import utility
 
-async def set_locale(command_info: utility.CommandInfo, locale: str) -> None:
+async def set_locale(command_info: utility.CommandInfo, locale_str: str) -> None:
     if isinstance(command_info.user, discord.Member) and isinstance(command_info.channel, discord.abc.GuildChannel) and (not command_info.channel.permissions_for(command_info.user).manage_guild):
         embed = utility.tanjunEmbed(title=locale.commands.admin.setLocale.missingPermission.title(str(command_info.locale)), description=locale.commands.admin.setLocale.missingPermission.description(str(command_info.locale)))
         await command_info.reply(embed=embed)
         return
     if command_info.guild is None:
         raise ValueError('Guild is missing in command_info')
-    await command_info.guild.edit(preferred_locale=locale, reason=locale.commands.admin.setLocale.setLocaleReason(str(command_info.locale)))
+    await command_info.guild.edit(preferred_locale=locale_str, reason=locale.commands.admin.setLocale.setLocaleReason(str(command_info.locale)))
     embed = utility.tanjunEmbed(title=locale.commands.admin.setLocale.success.title(str(command_info.locale)), description=locale.commands.admin.setLocale.success.description(str(command_info.locale)))
     await command_info.reply(embed=embed)
     return None
